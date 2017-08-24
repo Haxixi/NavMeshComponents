a769dbcfd72606ee1db71e7d95bb43632bbdb26a
nothing <287031517@qq.com>
2017-08-24 10:37:13 +0800

    初始化提交

---
+++ Assets/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Test : MonoBehaviour
+{
+    public NavMeshAgent NacMeshAgent;
+
+    public Transform pos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            //NacMeshAgent.SetDestination(pos.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+                NacMeshAgent.SetDestination(hit.point);
+        }
+    }
+}
